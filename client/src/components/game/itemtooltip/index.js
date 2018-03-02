32e8c296f123dd6c63a8faa40d978067d7e18017
Dennis Rasmussen <mail@dennisrasmussen.dk>
2018-03-03 00:56:47 +0100

    Cleaning up ItemTooltip

--- client/src/components/game/itemtooltip/index.js
+++ client/src/components/game/itemtooltip/index.js
@@ -4,31 +4,28 @@ import ReactDOM from 'react-dom';
 class ItemTooltip extends React.Component {
     constructor(props) {
         super(props);
-
-        this.item = this.props.item;
-        this.elm = document.createElement('div');
+        this.portalElement = document.createElement('div');
     }
 
     componentWillMount() {
-        console.log(this.props.coords);
-        document.body.appendChild(this.elm);
+        document.body.appendChild(this.portalElement);
     }
 
     componentWillUnmount() {
-        document.body.removeChild(this.elm);
+        document.body.removeChild(this.portalElement);
     }
 
     render() {
-        if (! this.item) {
+        if (! this.props.item) {
             return '';
         }
 
         return ReactDOM.createPortal(
             <div className="item-tooltip" style={{top: (this.props.coords.y + 10) + 'px', left: (this.props.coords.x + 10) + 'px'}}>
-                <div className="name">{this.item.name}</div>
-                <div className="count">Count: {this.item.count}</div>
+                <div className="name">{this.props.item.name}</div>
+                <div className="count">Count: {this.props.item.count}</div>
             </div>
-            , this.elm
+            , this.portalElement
         );
     }
 }
