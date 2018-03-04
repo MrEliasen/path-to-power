06b262c1b2082f19fce944703548daebf5049cf6
Mark Eliasen <hello@sirmre.com>
2018-03-04 19:13:47 +0000

    add missing email template(s) to data.sample

---
+++ server/game/data.sample/emails/activation.js
@@ -0,0 +1,9 @@
+module.exports = (link) => `<h1>PTP Account Activation</h1>
+
+<p>Thank you for signing up for player PTP! Before you can login, you will need to activate your account.
+To activate your account, click the link below, or copy paste it into your browser's address bar.</p>
+
+<p>Activation Link: <a href="${link}">${link}</a></p>
+
+<p>Welcome to PTP!</br>
+The PTP Team</p>`;
