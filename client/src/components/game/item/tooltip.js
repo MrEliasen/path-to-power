cc13d48424b61a630a6082f01c7ae9446c0d928c
Dennis Rasmussen <mail@dennisrasmussen.dk>
2018-03-11 22:44:14 +0100

    Added stats and description to item tooltips

--- client/src/components/game/item/tooltip.js
+++ client/src/components/game/item/tooltip.js
@@ -7,6 +7,21 @@ class ItemTooltip extends React.Component {
         this.portalElement = document.createElement('div');
     }
 
+    generateDescription(item) {
+        let description = item.description;
+
+        if (item.stats) {
+            Object.keys(item.stats).map((key) => {
+                if (typeof item.stats[key] !== 'object') {
+                    const exp = new RegExp(`({${key}})+`, 'gi');
+                    description = description.replace(exp, item.stats[key]);
+                }
+            });
+        }
+
+        return description;
+    }
+
     componentWillMount() {
         document.body.appendChild(this.portalElement);
     }
@@ -23,7 +38,20 @@ class ItemTooltip extends React.Component {
         return ReactDOM.createPortal(
             <div className="item-tooltip" style={{top: (this.props.coords.y + 10) + 'px', left: (this.props.coords.x + 10) + 'px'}}>
                 <div className="name">{this.props.item.name}</div>
-                <div className="count">Count: {this.props.item.count}</div>
+                <div className="description">{this.generateDescription(this.props.item)}</div>
+                <div className="stats">
+                    {
+                        Object.keys(this.props.item.stats).map((key, index) => {
+                            if (typeof this.props.item.stats[key] !== 'object') {
+                                return <div key={index}>{key}: {this.props.item.stats[key]}</div>;
+                            }
+                        })
+                    }
+                </div>
+                {
+                    this.props.item.count &&
+                    <div className="count">Count: {this.props.item.count}</div>
+                }
             </div>
             , this.portalElement
         );
