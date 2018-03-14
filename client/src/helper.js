eb4ee2948104c4c0eee860e3ef55fcd1303ade97
Mark Eliasen <hello@sirmre.com>
2018-03-14 21:56:32 +0000

    add cacheGet cacheSet to helper

--- client/src/helper.js
+++ client/src/helper.js
@@ -30,3 +30,43 @@ export function getStringColour(str) {
     }
     return colour;
 }
+
+/**
+ * Fetch a local storage value, if any, and if not expired.
+ * @param  {String} key The key of the local strorage value
+ * @return {Object|String|Number} The stored data for that key
+ */
+export function cacheGet(key) {
+    try {
+        let cache = window.localStorage.getItem(key);
+
+        if (!cache) {
+            throw new Error('Not found.');
+        }
+
+        cache = JSON.parse(cache);
+
+        if (cache.expire && cache.expire < new Date().getTime() / 1000) {
+            throw new Error('Cache expired.');
+        }
+
+        return cache.data;
+    } catch (err) {
+        return null;
+    }
+}
+
+/**
+ * Cache data to local storage. Expiry optional
+ * @param  {String} key    The key to store the data under
+ * @param  {Mixed}  data   The data to store
+ * @param  {Number} expire (Optional) how long the cache should remain valid (seconds)
+ */
+export function cacheSet(key, data, expire = null) {
+    let storeValue = {
+        expire: expire ? ((new Date().getTime() / 1000) + expire) : null,
+        data,
+    };
+
+    window.localStorage.setItem(key, JSON.stringify(storeValue));
+}
