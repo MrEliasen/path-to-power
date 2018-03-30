e5173f607daee2198adfe995f79d351d4b6c9af0
Mark Eliasen <hello@sirmre.com>
2018-03-31 00:01:59 +0100

    add shop filters

--- client/src/components/game/shop/index.js
+++ client/src/components/game/shop/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
-import {Alert, Row, Col, Modal, ModalHeader, ModalBody} from 'reactstrap';
+import {
+    Alert,
+    Row,
+    Col,
+    Modal,
+    ModalHeader,
+    ModalBody,
+    FormGroup,
+    Input,
+    Button,
+} from 'reactstrap';
 
 // components
 import ItemSlot from '../item/slot';
@@ -14,6 +24,12 @@ import {shopClose} from './actions';
 class Shop extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            itemType: '',
+            itemSubType: '',
+            sortBy: '',
+        };
     }
 
     renderNotification() {
@@ -39,6 +55,104 @@ class Shop extends React.Component {
         </Alert>;
     }
 
+    renderItemTypes() {
+        const {shop, itemList} = this.props;
+        let types = [];
+        let options = [];
+
+        shop.sell.list.forEach((shopItem) => {
+            const item = itemList[shopItem.id];
+
+            if (item && !types.includes(item.type)) {
+                types.push(item.type);
+                options.push(<option key={item.type} value={item.type}>{item.type}</option>);
+            }
+        });
+
+        return options;
+    }
+
+    renderItemSubTypes() {
+        const {itemType} = this.state;
+
+        if (itemType === '') {
+            return null;
+        }
+
+        const {shop, itemList} = this.props;
+        let types = [];
+        let options = [];
+
+        shop.sell.list.forEach((shopItem) => {
+            const item = itemList[shopItem.id];
+
+            if (item && !types.includes(item.subtype) && item.type === itemType) {
+                types.push(item.subtype);
+                options.push(<option key={item.subtype} value={item.subtype}>{item.subtype}</option>);
+            }
+        });
+
+        return options;
+    }
+
+    renderShopList() {
+        const {shop} = this.props;
+        let {itemType, itemSubType, sortBy} = this.state;
+
+        if (!shop) {
+            return null;
+        }
+
+        if (shop && !shop.sell.enabled) {
+            return <p>No selling anything.</p>;
+        }
+
+        let {list} = shop.sell;
+        let sortedItemList = [...list];
+
+        if (sortBy !== '') {
+            sortBy = sortBy.split(':');
+
+            sortedItemList = sortedItemList.sort((a, b) => {
+                if (a[sortBy[0]] < b[sortBy[0]]) {
+                    return sortBy[1] === 'asc' ? -1 : 1;
+                }
+
+                if (a[sortBy[0]] > b[sortBy[0]]) {
+                    return sortBy[1] === 'asc' ? 1 : -1;
+                }
+
+                return 0;
+            });
+        }
+
+        return sortedItemList.map((shopItem, index) => {
+            if (itemType !== '') {
+                const item = this.props.itemList[shopItem.id];
+
+                // if the item does not exist
+                if (!item) {
+                    return null;
+                }
+
+                // if the item type does not match
+                if (item.type !== itemType) {
+                    return null;
+                }
+
+                // if the item sub type is defined and does not match
+                if (itemSubType !== '' && item.subtype !== itemSubType) {
+                    return null;
+                }
+            }
+
+            return <SaleItem
+                key={index}
+                shopFingerprint={shop.fingerprint}
+                shopItem={shopItem} />;
+        });
+    }
+
     render() {
         const {shop, inventorySize} = this.props;
         const slots = Array.from(Array(inventorySize).keys());
@@ -49,19 +163,83 @@ class Shop extends React.Component {
                 <ModalBody>
                     {this.renderNotification()}
                     <Row>
+                        {
+                            shop && shop.sell.enabled &&
+                            <React.Fragment>
+                                <Col xs="12">
+                                    <strong>Shop Filters:</strong>
+                                </Col>
+                                <Col xs="3">
+                                    <FormGroup>
+                                        <Input
+                                            type="select"
+                                            name="type"
+                                            value={this.state.itemType}
+                                            onChange={(e) => this.setState({itemType: e.target.value, itemSubType: ''})}
+                                        >
+                                            <option value="" defaultValue hidden>Item Type</option>
+                                            {this.renderItemTypes()}
+                                        </Input>
+                                    </FormGroup>
+                                </Col>
+                                <Col xs="3">
+                                    <FormGroup>
+                                        <Input
+                                            type="select"
+                                            name="subtype"
+                                            value={this.state.itemSubType}
+                                            onChange={(e) => this.setState({itemSubType: e.target.value})}
+                                        >
+                                            <option value="" defaultValue hidden>Item Sub-Type</option>
+                                            {this.renderItemSubTypes()}
+                                        </Input>
+                                    </FormGroup>
+                                </Col>
+                                <Col xs="3">
+                                    <FormGroup>
+                                        <Input
+                                            type="select"
+                                            name="sortBy"
+                                            value={this.state.sortBy}
+                                            onChange={(e) => this.setState({sortBy: e.target.value})}
+                                        >
+                                            <option value="" defaultValue hidden>Sort By</option>
+                                            <optgroup label="Name">
+                                                <option value="name:asc">Ascending</option>
+                                                <option value="name:desc">Descending</option>
+                                            </optgroup>
+                                            <optgroup label="Price">
+                                                <option value="price:asc">Ascending</option>
+                                                <option value="price:desc">Descending</option>
+                                            </optgroup>
+                                            <optgroup label="EXP Needed">
+                                                <option value="expRequired:asc">Ascending</option>
+                                                <option value="expRequired:desc">Descending</option>
+                                            </optgroup>
+                                        </Input>
+                                    </FormGroup>
+                                </Col>
+                                <Col xs="3">
+                                    <Button
+                                        onClick={() => this.setState({
+                                            itemType: '',
+                                            itemSubType: '',
+                                            sortBy: '',
+                                        })}
+                                        color="warning"
+                                    >
+                                        Reset
+                                    </Button>
+                                </Col>
+                                <Col xs="12">
+                                    <hr style={{margin: '0px 0px 12px'}}/>
+                                </Col>
+                            </React.Fragment>
+                        }
                         <Col xs="6">
                             <div id="shop">
                                 {
-                                    shop && shop.sell.list.map((shopItem, index) =>
-                                        <SaleItem
-                                            key={index}
-                                            shopFingerprint={shop.fingerprint}
-                                            shopItem={shopItem} />
-                                    )
-                                }
-                                {
-                                    shop && !shop.sell.enabled &&
-                                    <p>No selling anything.</p>
+                                    this.renderShopList()
                                 }
                             </div>
                         </Col>
@@ -90,6 +268,7 @@ function mapDispatchToProps(dispatch) {
 
 function mapStateToProps(state) {
     return {
+        itemList: state.game.items,
         shop: state.shop,
         isOpen: state.shop ? state.shop.open : false,
         inventorySize: state.character.selected ? state.character.selected.stats.inventorySize : 0,
