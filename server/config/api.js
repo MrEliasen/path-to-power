280e6490cebe0d082b9c2fa29b95128fe270d9fb
Dennis Rasmussen <mail@dennisrasmussen.dk>
2018-04-14 18:22:53 +0200

    Fixing .gitignore to ignore the config folder

--- server/config/api.js
+++
@@ -1,6 +0,0 @@
-import {env} from '../utils/configure';
-
-export default {
-    url: env('API_URL', 'http://localhost'),
-    port: env('API_PORT', 8087),
-};
--- server/config/app.js
+++
@@ -1,6 +0,0 @@
-import {env} from '../utils/configure';
-
-export default {
-    clientUrl: env('APP_CLIENT_URL', 'http://localhost:8080'),
-    serverPort: env('APP_SERVER_PORT', 8086),
-};
