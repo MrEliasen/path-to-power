13f197dfdbc8fda505ca750d8bb8bc9191519e29
SirMrE <mark.eliasen@zolidsolutions.com>
2018-05-04 00:00:30 +0100

    add teleport dev command

--- server/src/components/command/commands.js
+++ server/src/components/command/commands.js
@@ -1,4 +1,4 @@
-import {COMMAND_CHAT_MESSAGE} from 'shared/actionTypes';
+import {COMMAND_CHAT_MESSAGE, CHARACTER_LEFT_GRID} from 'shared/actionTypes';
 
 /**
  * Check if there is an active cooldown or not, for chatting
@@ -204,7 +204,87 @@ function cmdInfo(socket, character, command, params, cmdObject, Game) {
     );
 }
 
+/**
+ * Teleport command logic
+ * @param  {Socket.io Socket} socket    The socket of the client who sent the command
+ * @param  {[type]} character           Character of the client sending the request
+ * @param  {String} command             the command eg. /say
+ * @param  {Object} params              The validated and parsed parameters for the command
+ * @param  {Object} cmdObject           The command object template
+ * @param  {Game}   Game                The main Game object
+ */
+function cmdTeleport(socket, character, command, params, cmdObject, Game) {
+    if (process.env.NODE_ENV !== 'development') {
+        return;
+    }
+
+    let y = params[0];
+    let x = params[1];
+    let gameMap = params[2].id || character.location.map;
+
+    // remove aim from current target, if set
+    character.releaseTarget();
+
+    // leave the old grid room
+    socket.leave(character.getLocationId());
+
+    // dispatch leave message to grid
+    Game.eventToRoom(character.getLocationId(), 'info', `You see ${character.name} vanish into thin air.`);
+
+    // remove player from the grid list of players
+    Game.socketManager.dispatchToRoom(character.getLocationId(), {
+        type: CHARACTER_LEFT_GRID,
+        payload: character.user_id,
+    });
+
+    // update character location
+    character.updateLocation(gameMap, x, y);
+
+    // dispatch join message to new grid
+    Game.eventToRoom(character.getLocationId(), 'info', `${character.name} emerge from thin air`, [character.user_id]);
+
+    // add player from the grid list of players
+    Game.socketManager.dispatchToRoom(
+        character.getLocationId(),
+        Game.characterManager.joinedGrid(character)
+    );
+
+    // update the socket room
+    socket.join(character.getLocationId());
+
+    // update client/socket character and location information
+    Game.characterManager.updateClient(character.user_id);
+
+    // send the new grid details to the client
+    Game.mapManager.updateClient(character.user_id);
+}
+
 module.exports = [
+    {
+        command: '/teleport',
+        aliases: [
+            '/tp',
+        ],
+        params: [
+            {
+                name: 'North Coordinate',
+                desc: 'The N coordinate to go to.',
+                rules: 'required|integer',
+            },
+            {
+                name: 'East Coordinate',
+                desc: 'The E coordinate to go to.',
+                rules: 'required|integer',
+            },
+            {
+                name: 'Map Name',
+                desc: 'The name of the map to go to.',
+                rules: 'gamemap',
+            },
+        ],
+        description: 'Teleport to a specific location.',
+        method: cmdTeleport,
+    },
     {
         command: '/global',
         aliases: [
